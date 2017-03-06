c8c11652cd7c45416613ac069353d632bb01f0d6
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-03-06 12:55:52 +0100

    Remove icon from vision tool

--- src/VisionTool.js
+++ src/VisionTool.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import SanityVision from './SanityVision'
 import {route} from 'part:@sanity/base/router'
 
@@ -6,8 +5,5 @@ export default {
   router: route('/*'),
   name: 'vision',
   title: 'Vision',
-  icon: function VisionIcon() {
-    return <div />
-  },
   component: SanityVision
 }
