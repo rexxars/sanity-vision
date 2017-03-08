70d8b119309ec54b88640d8ff76b54e79546b15e
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-03-08 09:39:08 +0100

    Add an actual icon for vision

--- src/VisionTool.js
+++ src/VisionTool.js
@@ -1,13 +1,11 @@
-import React from 'react'
-import SanityVision from './SanityVision'
 import {route} from 'part:@sanity/base/router'
-
-const icon = () => <div />
+import SanityVision from './SanityVision'
+import VisionIcon from './components/VisionIcon'
 
 export default {
   router: route('/*'),
   name: 'vision',
   title: 'Vision',
-  icon: icon,
+  icon: VisionIcon,
   component: SanityVision
 }
